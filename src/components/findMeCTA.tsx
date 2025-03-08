3bcd4f0138f23326a5fb9633d5c187623a4a386c
LHW001abc <abdelmajidzaddi088@gmail.com>
2025-03-08 18:18:46 +0000

    fix

--- src/components/findMeCTA.tsx
+++ src/components/findMeCTA.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { socials } from "@/config/app";
 import { useState } from "react";
-import Image from 'next/image';
 
 const FindMeCTA = () => {
   const [hoveredIcon, setHoveredIcon] = useState<string | null>(null);
